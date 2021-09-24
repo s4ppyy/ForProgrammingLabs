b7b1ebe394bee0470a2fad677fb53af1166df483
s4ppyy <91278174+s4ppyy@users.noreply.github.com>
2021-09-24 16:24:59 +0300

    Update programming_Laba1.cpp

--- programming_Laba1/programming_Laba1.cpp
+++ programming_Laba1/programming_Laba1.cpp
@@ -3,9 +3,9 @@
 int main()
 {
 	float var;
-	char simvol;
-	scanf_s("%c", &simvol);
-	printf("%c", simvol);
+	char symbol;
+	scanf_s("%c", &symbol);
+	printf("%c", symbol);
 	scanf_s("%f", &var);
 	printf("%f", var);
 	return 0;
