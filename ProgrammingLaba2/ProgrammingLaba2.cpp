776eedffdd146fa64b6507f6ca30cf2f8bbcf7fa
s4ppyy <91278174+s4ppyy@users.noreply.github.com>
2021-09-24 09:07:50 +0300

    Update ProgrammingLaba2.cpp

--- ProgrammingLaba2/ProgrammingLaba2.cpp
+++ ProgrammingLaba2/ProgrammingLaba2.cpp
@@ -6,7 +6,7 @@ int main()
     float m1, m2, z1, z2;
     scanf_s("%f", &m1);
     scanf_s("%f", &m2);
-    if ((sqrt(pow((3 * m1 + 2), 2) - 24 * m1))> 0.67) 
+    if ((sqrt(pow((3 * m1 + 2), 2) - 24 * m1))> 0) 
     {
         z1 = (sqrt(pow((3 * m1 + 2), 2) - 24 * m1)) / (3 * sqrt(m1) - (2 / sqrt(m1)));
         printf("%f", z1);
