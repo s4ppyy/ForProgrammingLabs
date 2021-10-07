4411c931032b837f8f3c7ccaffb45b621f464c3b
s4ppyy <91278174+s4ppyy@users.noreply.github.com>
2021-10-07 22:27:52 +0300

    Add files via upload

---
+++ Programming3/cmake-build-debug/CMakeFiles/Programming3.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Programming3.dir/main.c.obj"
+  "Programming3.exe"
+  "Programming3.exe.manifest"
+  "Programming3.lib"
+  "Programming3.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Programming3.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
