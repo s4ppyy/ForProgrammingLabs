9a0794a5610f3e38fa7afafc5dbc17170b2ae6e1
s4ppyy <91278174+s4ppyy@users.noreply.github.com>
2021-11-29 23:16:51 +0300

    Add files via upload

--- main.c
+++ main.c
@@ -1,28 +1,50 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+int* distance(int* x_coord, int* y_coord, int n) {
+    int* array = malloc(n*(n-1) * sizeof(int));
+    int counter = 0;
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            if (i != j) {
+                array[counter] = sqrt(pow(abs(x_coord[i] - x_coord[j]), 2) + pow(abs(y_coord[i] - y_coord[j]), 2));
+                counter += 1;
+            }
+        }
+    }
+    return array;
+}
+
+int counter(int number){
+    if(!number)
+        return 0;
+    return (number % 10) + counter(number / 10);
+    }
 
 int main() {
-    int n = 7;
-    int array[7] = {90, 76, 54, 23, 56, 12, 48};
-    for (int i = 0; i < n; ++i) {
-        printf("%d ", array[i]);
+    int n;
+    printf("Insert array length: \n");
+    scanf("%d", &n);
+    printf("Enter n coordinate pairs:\n");
+    int* arrX = malloc(n * sizeof(int));
+    int* arrY = malloc(n * sizeof(int));
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &arrX[i]);
+        scanf("%d", &arrY[i]);
     }
-    int lenght1 = 4;
-    int matrix1[2][2] = {1, 2, 3, 4};
-    int lenght2 = 4;
-    int matrix2[2][2] = {1, 0, 0, 1};
-    int lenght3 = 4;
-    int multi[2][2] = {0};
-    for(int i = 0; i < 2; i++)
-        for(int j = 0; j < 2; j++) {
-            multi[i][j] = 0;
-            for(int k = 0; k < 2; k++)
-                multi[i][j] += matrix1[i][k] * matrix2[k][j];
+    int* array = distance(arrX, arrY, n);
+    for (int i = 0; i < n*(n-1); i++) {
+        if (i != n*(n-1)-1) {
+            printf("%d ", array[i]);
+        } else {
+            printf("%d\n", array[i]);
         }
-    printf("\n");
-    for (int i = 0; i < 2; ++i) {
-        for (int j = 0; j < 2; ++j)
-            printf("%d ", multi[i][j]);
-        printf("\n");
     }
+
+    int number;
+    printf("Insert your number: \n");
+    scanf("%d", &number);
+    printf("Sum of numbers is %d", counter(number));
     return 0;
 }
