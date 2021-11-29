c9edc84ff39021847063e65aad118703fba5b056
s4ppyy <91278174+s4ppyy@users.noreply.github.com>
2021-11-29 23:17:40 +0300

    Add files via upload

---
+++ LabProgramming10/cmake-build-debug/CMakeFiles/LabProgramming10.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/LabProgramming10.dir/main.c.obj"
+  "LabProgramming10.exe"
+  "LabProgramming10.exe.manifest"
+  "LabProgramming10.lib"
+  "LabProgramming10.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/LabProgramming10.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
